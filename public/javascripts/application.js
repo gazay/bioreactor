9b221d9ff49ca2282272c8e1fa8519d50512922b
brainopia <ravwar@gmail.com>
2010-10-16 11:16:16 +0400

    Import web-socket-js scripts

---
+++ public/javascripts/application.js
@@ -0,0 +1,11 @@
+WEB_SOCKET_SWF_LOCATION = "/javascripts/WebSocketMain.swf";
+WEB_SOCKET_DEBUG = false;
+
+socket = new WebSocket('ws://localhost:8080')
+
+socket.parse = function(data){ console.log('got message: ' + data) }
+
+socket.onopen = function(){ console.log('opened') }
+socket.onclose = function(){ console.log('closed') }
+socket.onerror = function(){ console.log('error') }
+socket.onmessage = function(event){ socket.parse(event.data) }
