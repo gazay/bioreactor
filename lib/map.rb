a778f1d48955d563cdf48b3cd5b300f609c48eb8
brainopia <ravwar@gmail.com>
2010-10-16 13:31:30 +0400

    Remove empty condition

--- lib/map.rb
+++ lib/map.rb
@@ -3,20 +3,18 @@ module Map
   width = 70
   height = 50
   @map = Array.new(width) { |x| Array.new(height) { |y| Cell.new x, y }}
-  
+
   def generate
     #TODO: when we will do some walls - don't forget this method
   end
-  
+
   def cell((x,y0), y1=nil)
     y = y0 || y1
     if x < width and y < height and x >= 0 and y >= 0
       @map[x][y]
-    else
-      nil
     end
   end
-  
+
   def get_random
     cell = @map.cell rand(width), rand(height)
     cell.content.nil? ? cell : get_random
