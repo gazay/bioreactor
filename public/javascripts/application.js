55da8d5b65d3bef838c9d89277ba1a54418016ab
brainopia <ravwar@gmail.com>
2010-10-16 16:01:30 +0400

    Test cookie setting via websocket

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,9 +3,25 @@ WEB_SOCKET_DEBUG = false;
 
 socket = new WebSocket('ws://bioreactor.r10.railsrumble.com:8080')
 
-socket.parse = function(data){ console.log('got message: ' + data) }
+socket.parse = function(data){
+    canvas.draw(JSON.parse(data))
+}
 
 socket.onopen = function(){ console.log('opened') }
 socket.onclose = function(){ console.log('closed') }
 socket.onerror = function(){ console.log('error') }
 socket.onmessage = function(event){ socket.parse(event.data) }
+
+canvas = $('canvas')[0].getContext('2d')
+canvas.draw = function(objects){
+    $.each(objects, function(index, object){
+        var id = object[0],
+            locations = object[1]
+
+        $.each(locations, function(index, location){
+            var x = location[0],
+                y = location[1]
+
+        })
+    })
+}
