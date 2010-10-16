8e7db9491659f58ad49a8ee9633784826ce3dd57
brainopia <ravwar@gmail.com>
2010-10-16 10:36:11 +0400

    Initial rails import

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Rr10Team69::Application.config.session_store :cookie_store, :key => '_rr10-team-69_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# Rr10Team69::Application.config.session_store :active_record_store
