5204c1340c70c1b205839c512263878d2f8be9e7
brainopia <ravwar@gmail.com>
2010-10-18 01:59:36 +0400

    Resume initial size of worm

--- lib/worm.rb
+++ lib/worm.rb
@@ -9,10 +9,6 @@ class Worm
     init_data
     @speed = 1
     @@worms[socket] = self
-    2.times do
-      cell = @cells.first.next(direction)
-      unshift cell if cell
-    end
   end
 
   def init_data
