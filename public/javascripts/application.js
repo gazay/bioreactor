0415b2689d96ff7987c74d3f6e8aa6992188f364
brainopia <ravwar@gmail.com>
2010-10-17 05:54:03 +0400

    Revert preventing sending info to active sockets

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -22,7 +22,6 @@ socket.onopen = function(){
     previousTime = new Date().getTime()
 }
 socket.onmessage = function(event){
-    socket.send('ready')
     currentTime = new Date().getTime()
     console.log((currentTime - previousTime)/1000)
     previousTime = currentTime
